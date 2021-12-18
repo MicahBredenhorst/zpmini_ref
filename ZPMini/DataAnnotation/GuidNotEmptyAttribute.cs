f56d5cf6b76a125a8fe334c66a3997af52e65a43
Micah <degrootmicah@gmail.com>
2021-12-18 18:14:25 +0100

    Added custom dataattribute for empty guid validation

---
+++ ZPMini/DataAnnotation/GuidNotEmptyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZPMini.API.DataAnnotation
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter,
+    AllowMultiple = false)]
+    public class GuidNotEmptyAttribute : ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "The {0} field must not be empty";
+        public GuidNotEmptyAttribute() : base(DefaultErrorMessage) { }
+        public override bool IsValid(object value)
+        {
+            if((Guid)value != Guid.Empty)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
